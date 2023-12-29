ccb954fcc4072a39565ab9559c31c17c5ea89f70
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2023-12-30 01:09:17 +0500

    msg

---
+++ script.js
@@ -0,0 +1,21 @@
+let guyName='johns';
+let  ladyRollNumber = 12345;
+
+let calcPercent = function(marks){
+return (marks/425)*100
+}
+
+const guyDocuments = {
+    result : {
+        math  : 23,
+        english : '23'
+    },
+    guysFriends:['yaseen','malik'],
+    marks : 234
+    
+}
+
+def= 'annuu';
+console.log(def);
+
+console.log(guyDocuments[`guysFriends`]);
