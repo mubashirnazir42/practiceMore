ed6572a35beee0cc09b9409286720dd86e85b1fd
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-04 01:24:49 +0500

    etension added

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-let guyName='johns';
-let  ladyRollNumber = 12345;
+'use strict'
+let guyName = 'johns'
+let ladyRollNumber = 12345
+console.log('hi')
+console.log('hi')
 
-let calcPercent = function(marks){
-return (marks/425)*100
+let calcPercent = function (marks) {
+  return (marks / 425) * 100
 }
 
 const guyDocuments = {
-    result : {
-        math  : 23,
-        english : '23'
-    },
-    guysFriends:['yaseen','malik'],
-    marks : 234
-    
+  result: {
+    math: 23,
+    english: '23',
+  },
+  guysFriends: ['yaseen', 'malik'],
+  marks: 234,
 }
 
-def= 'annuu';
-console.log(def);
+// def = 'annuu'
+// console.log(def)
 
-console.log(guyDocuments[`guysFriends`]);
+console.log(guyDocuments[`guysFriends`])
 
-
-const calc = function(){
-    let arr=[1,2,3,'mubashir']
-    console.log(arr.unshift(2));
-
+const calc = function () {
+  let arr = [1, 2, 3, 'mubashir']
+  console.log(arr.unshift(2))
 }
 
+const mama = []
+
+const myName = function () {}
 
 calc()
 
-
-
-const aboutMe={
-    calc :  function(){
-return 'lora'
-    }
+const aboutMe = {
+  calc: function () {
+    return 'lora'
+  },
 }
 
-console.log(aboutMe['calc']());
+console.log(aboutMe['calc']())
 
-console.log('helllo');
+console.log('helllo')
